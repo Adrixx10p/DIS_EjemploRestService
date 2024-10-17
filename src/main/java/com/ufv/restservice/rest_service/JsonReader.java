a27e796064b1c42141d3a51e53c989c40bea3d2d
Adrien <9105250@alumnos.ufv.es>
2024-10-17 20:23:06 +0200

    Terminada la lectura del json

--- src/main/java/com/ufv/restservice/rest_service/JsonReader.java
+++ src/main/java/com/ufv/restservice/rest_service/JsonReader.java
@@ -8,6 +8,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 
+
 public class JsonReader {
     public ArrayList<User> readJsonFile(String fichero){
         try {
